eafc0e893a4e80af51a5c4cb1baaafaf827cde50
ShenalMendis <mendisshenal@gmail.com>
2022-02-08 13:38:38 +0000

    3

--- IF STATEMENTS/banned_users.py
+++ IF STATEMENTS/banned_users.py
@@ -3,3 +3,7 @@ user = 'marie'
 
 if user not in banned_users:
     print(f"{user.title()}, you can post a response if you wish.")
+elif user in banned_users:
+    print(f"{user.title()}, BANNED.")
+else:
+    print("Error!")
