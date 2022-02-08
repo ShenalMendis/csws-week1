00592a3973c4e94fb06622fe9c24dbf9867a7cd8
ShenalMendis <mendisshenal@gmail.com>
2022-02-08 14:20:43 +0000

    thrid

---
+++ USER INPUTS & WHILE LOOPS/rollercoaster.py
@@ -0,0 +1,16 @@
+from sqlalchemy import false, true
+
+loop = True
+
+while loop:
+    
+    height = input("Height: ")
+    height = int(height)
+
+    if height >= 40:
+        print("Too tall for the ride!")
+    elif 20 <= height < 40:
+        print("Go Ahead!")
+        loop = False
+    else:
+        print("Too short!")
